987bcaf424ec66a2fd7ca48a6b17b8444c5b5f29
Kurt S Steffan <kurt.s.steffan@gmail.com>
2021-01-25 17:16:14 +0000

    Amended spelling mistake of Calculator class and file... and added tests for Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator(8,4);
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(12, calculator.add());
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(4, calculator.subtract());
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(32, calculator.multiply());
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(2,calculator.divide(), 0.0);
+    }
+
 }
